2190893126db28b94b2cb3b820f9043fd4424796
inaldopf <inaldofreitasjr@gmail.com>
2024-11-06 09:00:07 -0300

    remove r.delete

--- API/Predis/main.py
+++ API/Predis/main.py
@@ -4,7 +4,6 @@ from mongo.main import ActualMongosCart
 
 def setCart(cpf):
     # pega do mongo o joga no redis
-    r.delete(f"Cart:{cpf}")
     cartM = ActualMongosCart(cpf)
     FinalValue = cartM["FinalValue"]
     if len(r.hgetall(f"Cart:{cpf}").items()) <= 0 and len(cartM) > 0:
