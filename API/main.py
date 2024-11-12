fd5a8a62c097d99ff2d62f9bb481625e2407547e
inaldopf <inaldofreitasjr@gmail.com>
2024-11-12 10:31:56 -0300

    a

--- API/main.py
+++ API/main.py
@@ -221,7 +221,7 @@ def predictUser():
     return jsonify({"result": result})
 
 
-@app.route("/deleteCart", methods=["GET"])
+@app.route("/deleteCart", methods=["DELETE"])
 def deleteProductCart():
     cpf = request.args.get("cpf", type=str)
     product = request.args.get("product", type=str)
